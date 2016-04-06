17dc37d91c74a189173477341e0652d4bcad4b6a
peter.shi <532289386@qq.com>
2016-04-06 14:55:47 +0800

    Initial Commit

---
+++ TestOCR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOCR
+//
+//  Created by 赛峰 施 on 16/4/6.
+//  Copyright © 2016年 赛峰 施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
